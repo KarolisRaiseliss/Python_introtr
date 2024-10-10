cdd5b8382096d19a4b9d478e20046412a9ea8bb5
KarolisRaiseliss <karol.raiselis@gmail.com>
2024-10-10 11:03:23 +0300

    werfgh

---
+++ Skaičiai ir matematika.py
@@ -0,0 +1,21 @@
+# Step 1: 8 * 4 + 2
+step_1 = 8 * 4 + 2
+
+# Step 2: 8 * (4 + 2)
+step_2 = 8 * (4 + 2)
+
+# Step 3: 48 / 4
+step_3 = 48 / 4
+
+# Step 4: 3 + 6 * 2
+step_4 = 3 + 6 * 2
+
+# Output the results
+print("Step 1 result:", step_1)
+print("Step 2 result:", step_2)
+print("Step 3 result:", step_3)
+print("Step 4 result:", step_4)
+
+
+git config --global user.email "karol.raiselis@gmail.com"
+git config --global user.name 'KarolisRaiseliss'
---
+++ main.py
@@ -0,0 +1,16 @@
+print('hi')
+
+# Užduotis_1
+print('pirma uzd')
+tekstas = "labailabailabailabailabailabailabailabailabailabailabailabailabailabai"
+print(tekstas)
+#Tekstas yra kintamasis
+# Boolean yra true/false
+
+kint_1 = 'labas'
+kint_1 = 20
+print(kint_1)
+if True/False:
+    print("gal tiesa")
+
+
